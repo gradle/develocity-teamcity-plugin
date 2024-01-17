41d0d769c4dba9f5d23b230d14cbd9916f73cdbc
Iurii Ignatko <iignatko@gradle.com>
2024-01-17 09:52:39 +0200

    Rename plugin to Develocity in code

---
+++ agent/src/test/groovy/com/gradle/develocity/maven/TeamCityGoalFilteringExtensionApplicationTest.groovy
@@ -0,0 +1,41 @@
+package com.gradle.develocity.maven
+
+import com.gradle.develocity.TcPluginConfig
+import com.gradle.develocity.maven.testutils.MavenBuildStepConfig
+import com.gradle.develocity.maven.testutils.MavenProject
+
+import static org.junit.Assume.assumeTrue
+
+class TeamCityGoalFilteringExtensionApplicationTest extends BaseExtensionApplicationTest {
+
+    def "does not publish build scan for TeamCity specific info goal invocation (#jdkCompatibleMavenVersion)"() {
+        assumeTrue jdkCompatibleMavenVersion.isJvmVersionCompatible()
+        assumeTrue GE_URL != null
+
+        given:
+        def mvnProject = new MavenProject.Configuration().buildIn(checkoutDir)
+
+        and:
+        def gePluginConfig = new TcPluginConfig(
+                develocityUrl: GE_URL,
+                develocityExtensionVersion: GE_EXTENSION_VERSION,
+        )
+
+        and:
+        def mvnBuildStepConfig = new MavenBuildStepConfig(
+            checkoutDir: checkoutDir,
+            goals: 'org.jetbrains.maven:info-maven3-plugin:1.0.2:info',
+        )
+
+        when:
+        def output = run(jdkCompatibleMavenVersion.mavenVersion, mvnProject, gePluginConfig, mvnBuildStepConfig)
+
+        then:
+        outputMissesTeamCityServiceMessageBuildStarted(output)
+        outputMissesTeamCityServiceMessageBuildScanUrl(output)
+
+        where:
+        jdkCompatibleMavenVersion << SUPPORTED_MAVEN_VERSIONS
+    }
+
+}
---
+++ agent/src/test/groovy/com/gradle/develocity/maven/UrlConfigurationExtensionApplicationTest.groovy
@@ -0,0 +1,107 @@
+package com.gradle.develocity.maven
+
+import com.gradle.develocity.TcPluginConfig
+import com.gradle.develocity.maven.testutils.MavenProject
+
+import static org.junit.Assume.assumeTrue
+
+class UrlConfigurationExtensionApplicationTest extends BaseExtensionApplicationTest {
+
+    def "ignores GE URL requested via TC config when GE extension is not applied via the classpath (#jdkCompatibleMavenVersion)"() {
+        assumeTrue jdkCompatibleMavenVersion.isJvmVersionCompatible()
+        assumeTrue GE_URL != null
+
+        given:
+        def mvnProject = new MavenProject.Configuration(
+            geUrl: GE_URL,
+            geExtensionVersion: GE_EXTENSION_VERSION,
+        ).buildIn(checkoutDir)
+
+        and:
+        def gePluginConfig = new TcPluginConfig(
+                develocityUrl: new URI('https://develocity-server.invalid'),
+                develocityAllowUntrustedServer: true,
+                develocityExtensionVersion: GE_EXTENSION_VERSION,
+        )
+
+        when:
+        def output = run(jdkCompatibleMavenVersion.mavenVersion, mvnProject, gePluginConfig)
+
+        then:
+        0 * extensionApplicationListener.geExtensionApplied(_)
+        0 * extensionApplicationListener.ccudExtensionApplied(_)
+
+        and:
+        outputContainsTeamCityServiceMessageBuildStarted(output)
+        outputContainsTeamCityServiceMessageBuildScanUrl(output)
+
+        where:
+        jdkCompatibleMavenVersion << SUPPORTED_MAVEN_VERSIONS
+    }
+
+    def "configures GE URL requested via TC config when GE extension is applied via classpath (#jdkCompatibleMavenVersion)"() {
+        assumeTrue jdkCompatibleMavenVersion.isJvmVersionCompatible()
+        assumeTrue GE_URL != null
+
+        given:
+        def mvnProject = new MavenProject.Configuration(
+            geUrl: 'https://develocity-server.invalid',
+            geExtensionVersion: null,
+        ).buildIn(checkoutDir)
+
+        and:
+        def gePluginConfig = new TcPluginConfig(
+                develocityUrl: GE_URL,
+                develocityAllowUntrustedServer: true,
+                develocityExtensionVersion: GE_EXTENSION_VERSION,
+        )
+
+        when:
+        def output = run(jdkCompatibleMavenVersion.mavenVersion, mvnProject, gePluginConfig)
+
+        then:
+        1 * extensionApplicationListener.geExtensionApplied(GE_EXTENSION_VERSION)
+        0 * extensionApplicationListener.ccudExtensionApplied(_)
+
+        and:
+        outputContainsTeamCityServiceMessageBuildStarted(output)
+        outputContainsTeamCityServiceMessageBuildScanUrl(output)
+
+        where:
+        jdkCompatibleMavenVersion << SUPPORTED_MAVEN_VERSIONS
+    }
+
+    def "enforces GE URL and allowUntrustedServer in project if enforce url parameter is enabled (#jdkCompatibleMavenVersion)"() {
+        assumeTrue jdkCompatibleMavenVersion.isJvmVersionCompatible()
+        assumeTrue GE_URL != null
+
+        given:
+        def mvnProject = new MavenProject.Configuration(
+                geUrl: new URI('https://develocity-server.invalid'),
+                geExtensionVersion: GE_EXTENSION_VERSION,
+        ).buildIn(checkoutDir)
+
+        and:
+        def gePluginConfig = new TcPluginConfig(
+                develocityUrl: GE_URL,
+                develocityAllowUntrustedServer: true,
+                develocityEnforceUrl: true,
+                develocityExtensionVersion: GE_EXTENSION_VERSION,
+        )
+
+        when:
+        def output = run(jdkCompatibleMavenVersion.mavenVersion, mvnProject, gePluginConfig)
+
+        then:
+        0 * extensionApplicationListener.geExtensionApplied(_)
+        0 * extensionApplicationListener.ccudExtensionApplied(_)
+
+        and:
+        outputContainsTeamCityServiceMessageBuildStarted(output)
+        outputContainsTeamCityServiceMessageBuildScanUrl(output)
+
+        where:
+        jdkCompatibleMavenVersion << SUPPORTED_MAVEN_VERSIONS
+    }
+
+}
